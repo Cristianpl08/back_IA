452ff1ec2630f27cd7d02e6cfc09898cb2bb11ca
Cristianpl08 <cristian.plazas534@gmail.com>
2024-10-14 19:59:35 -0500

    test 1 all users

--- app.py
+++ app.py
@@ -2,31 +2,32 @@ from flask import Flask, request, jsonify
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from flask_cors import CORS
-
+from routes import api_bp  # Asegúrate de que api_bp está importado correctamente
 app = Flask(__name__)
 CORS(app)  # Esto habilitará CORS para todas las rutas
 
-CLIENT_ID = "277375272976-pkvaiiom7klc9mubv2aj8lbrtroqvb7i.apps.googleusercontent.com"  # Usa tu client_id
+CLIENT_ID = "611668385896-jeom4mshdeqc55rh8hfs2bgi6dnka1q3.apps.googleusercontent.com"  # cristianp app
 
-@app.route('/login', methods=['POST'])
+@app.route('/api/login', methods=['POST', 'OPTIONS'])
 def login():
+    if request.method == 'OPTIONS':
+        return '', 200  # Responder a preflight
+
     token = request.json.get('token')
-    
+
     try:
         # Verificar el token con Google
         idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-
-        # ID token is valid. Get the user's Google Account info.
         userid = idinfo['sub']
         email = idinfo['email']
         name = idinfo.get('name')
 
-        # Aquí podrías implementar la lógica para manejar usuarios en tu backend
         return jsonify({"message": "Login exitoso", "userid": userid, "email": email, "name": name}), 200
 
     except ValueError:
-        # El token es inválido
         return jsonify({"error": "Invalid token"}), 401
+
+app.register_blueprint(api_bp, url_prefix='/api')  # Usa un prefijo para evitar confusiones
 
 if __name__ == '__main__':
     app.run(port=5005, debug=True)
